a9472a4ec896e22a0ef0fae5c6b41415c25d071c
Derek Liang <frostyrusty@gmail.com>
2014-08-18 13:26:49 -0700

    added tablet specific layouts

--- src/com/frostyrusty/ribbit/SignUpActivity.java
+++ src/com/frostyrusty/ribbit/SignUpActivity.java
@@ -20,43 +20,53 @@ public class SignUpActivity extends Activity {
 	protected EditText mPassword;
 	protected EditText mEmail;
 	protected Button mSignUpButton;
-	
+	protected Button mCancelButton;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 		setContentView(R.layout.activity_sign_up);
-		
+
 		// hide the action bar
 		ActionBar actionBar = getActionBar();
 		actionBar.hide();
-		
+
 		mUsername = (EditText) findViewById(R.id.usernameField);
 		mPassword = (EditText) findViewById(R.id.passwordField);
 		mEmail = (EditText) findViewById(R.id.emailField);
+		mCancelButton = (Button) findViewById(R.id.cancelButton);
+		mCancelButton.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
+
 		mSignUpButton = (Button) findViewById(R.id.signupButton);
 		mSignUpButton.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				// Verify the valid values
 				String username = mUsername.getText().toString();
 				String password = mPassword.getText().toString();
 				String email = mEmail.getText().toString();
-				
+
 				username = username.trim();
 				password = password.trim();
 				email = email.trim();
-				
+
 				if (username.isEmpty() || password.isEmpty() || email.isEmpty()) {
-					AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+					AlertDialog.Builder builder = new AlertDialog.Builder(
+							SignUpActivity.this);
 					builder.setMessage(R.string.sign_up_error_message)
-						.setTitle(R.string.sign_up_error_title)
-						.setPositiveButton(android.R.string.ok, null);
+							.setTitle(R.string.sign_up_error_title)
+							.setPositiveButton(android.R.string.ok, null);
 					AlertDialog dialog = builder.create();
 					dialog.show();
-				}
-				else {
+				} else {
 					setProgressBarIndeterminateVisibility(true);
 					ParseUser newUser = new ParseUser();
 					newUser.setUsername(username);
@@ -68,16 +78,18 @@ public class SignUpActivity extends Activity {
 							setProgressBarIndeterminateVisibility(false);
 							if (e == null) {
 								// Success!
-								Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+								Intent intent = new Intent(SignUpActivity.this,
+										MainActivity.class);
 								intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 								intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
 								startActivity(intent);
-							}
-							else {
-								AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+							} else {
+								AlertDialog.Builder builder = new AlertDialog.Builder(
+										SignUpActivity.this);
 								builder.setMessage(e.getMessage())
-									.setTitle(R.string.sign_up_error_title)
-									.setPositiveButton(android.R.string.ok, null);
+										.setTitle(R.string.sign_up_error_title)
+										.setPositiveButton(android.R.string.ok,
+												null);
 								AlertDialog dialog = builder.create();
 								dialog.show();
 							}
